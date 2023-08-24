7be0f064376d38f8bfb2ee118d0d3ddd24bb0eb3
Sarthak <srnegi141@gmail.com>
2023-08-24 14:50:35 -0500

    restructured

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Commentalyzer (Still under construction...)
+
 Fetches all the comments from a youtube video and performs sentiment analysis.
 
 Uses the YouTube API to get all the comments from the video the link was provided for.
--- script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-  alert("Overall, the viewers' emotional inclination is towards seeking help and expressing gratitude. They are interested in learning and improving their skills, but they also encounter challenges and frustrations along the way, which they express in their comments. However, despite their difficulties, they are generally appreciative of the content creators and express gratitude for the help they receive.");
-});
